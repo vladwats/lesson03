7ba9a0ccc35da0947615652e6c3d5a6f2fdede4f
vladwats <106876485+vladwats@users.noreply.github.com>
2023-02-12 13:35:49 +0300

    Update Task05.java

--- src/lt/lhu/unit02/main/Task05.java
+++ src/lt/lhu/unit02/main/Task05.java
@@ -6,7 +6,7 @@ public class Task05 {
 		double a = 2; 
 		double b = 100; 
 		double c = 15;
-		double F = (b+Math.sqrt(Math.pow(b, 2)+4*a*c))/2*a-(Math.pow(a, 3)*c)+b;
+		double F = (b+Math.sqrt(Math.pow(b, 2)+4*a*c))/(2*a)-(Math.pow(a, 3)*c)+b;
 		System.out.println(F);
 
 	}
